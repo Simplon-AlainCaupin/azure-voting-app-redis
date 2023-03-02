0832eeb361bb5e88cad807f3fe5dc301cd14f5b2
Simplon-AlainCaupin <108000871+Simplon-AlainCaupin@users.noreply.github.com>
2023-03-02 14:16:19 +0100

    Update qalscript.sh

--- qalscript.sh
+++ qalscript.sh
@@ -1,5 +1,11 @@
-verdocker=$((curl 'https://hub.docker.com/v2/repositories/alaincpn/voteapp/tags' | jq '."results"[0]["name"]')| sed 's/\"//g')
-echo "##vso[task.setvariable variable=verdock]$verdocker"
-echo $verdocker
-prev=$(echo $KUBE_KUBECTLOUTPUT | jq '.items[1].spec.template.spec.containers[].image' | cut -d: -f2 | sed 's/"//')
-echo $prev
+#verdocker=$((curl 'https://hub.docker.com/v2/repositories/alaincpn/voteapp/tags' | jq '."results"[0]["name"]')| sed 's/\"//g')
+#echo "##vso[task.setvariable variable=verdock]$verdocker"
+#echo $verdocker
+#prev=$(echo $KUBE_KUBECTLOUTPUT | jq '.items[1].spec.template.spec.containers[].image' | cut -d: -f2 | sed 's/"//')
+#echo $prev
+
+versionnew=$((curl 'https://hub.docker.com/v2/repositories/alaincpn/voteapp/tags' | jq '."results"[0]["name"]')| sed 's/\"//g')
+versionold=$(echo $KUBE_KUBECTLOUTPUT | jq '.items[1].spec.template.spec.containers[].image' | cut -d: -f2 | sed 's/"//')
+echo "##vso[task.setvariable variable=vernew]$versionnew"
+echo "##vso[task.setvariable variable=verold]$versionold"
+sed -i 's/{{ version }}/'$versionnew'/g' app-deploy.yml
