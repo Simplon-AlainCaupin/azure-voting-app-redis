beef8b23e78f74905fbf93535269102affa7c42f
Simplon-AlainCaupin <108000871+Simplon-AlainCaupin@users.noreply.github.com>
2023-03-02 11:49:14 +0100

    Update qalscript.sh

--- qalscript.sh
+++ qalscript.sh
@@ -1,5 +1,5 @@
 verdocker=$((curl 'https://hub.docker.com/v2/repositories/alaincpn/voteapp/tags' | jq '."results"[0]["name"]')| sed 's/\"//g')
 echo "##vso[task.setvariable variable=verdock]$verdocker"
 echo $verdocker
-prev=$(echo $KUBE_KUBECTLOUTPUT | jq '.items[1].spec.template.spec.containers[].image')
+prev=$(echo $KUBE_KUBECTLOUTPUT)
 echo $prev
