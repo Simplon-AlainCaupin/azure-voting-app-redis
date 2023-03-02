3b15e9dc287b00691aea64a7ca87d0479c811d67
Simplon-AlainCaupin <108000871+Simplon-AlainCaupin@users.noreply.github.com>
2023-03-02 14:26:20 +0100

    Update qalscript.sh

--- qalscript.sh
+++ qalscript.sh
@@ -6,6 +6,10 @@
 
 versionnew=$((curl 'https://hub.docker.com/v2/repositories/alaincpn/voteapp/tags' | jq '."results"[0]["name"]')| sed 's/\"//g')
 versionold=$(echo $KUBE_KUBECTLOUTPUT | jq '.items[1].spec.template.spec.containers[].image' | cut -d: -f2 | sed 's/"//')
+echo $versionnew
+echo $versionold
+echo
 echo "##vso[task.setvariable variable=vernew]$versionnew"
 echo "##vso[task.setvariable variable=verold]$versionold"
+echo
 sed -i 's/{{ version }}/'$versionnew'/g' ./app/app.yml
